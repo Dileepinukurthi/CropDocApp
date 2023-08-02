1909920a4ef2d38a1e4b8aac9b5b7f854bb4abd1
Inukurthi Dileep <88875532+Dileepinukurthi@users.noreply.github.com>
2023-08-02 09:22:29 +0530

    Integrated with API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -11,18 +14,28 @@ void main() {
 
 class CameraAndFilePickerButton extends StatefulWidget {
   @override
-  _CameraAndFilePickerButtonState createState() => _CameraAndFilePickerButtonState();
+  _CameraAndFilePickerButtonState createState() =>
+      _CameraAndFilePickerButtonState();
 }
 
-class _CameraAndFilePickerButtonState extends State<CameraAndFilePickerButton> {
+class _CameraAndFilePickerButtonState
+    extends State<CameraAndFilePickerButton> {
   File? _capturedImage;
   String _fileName = "No file selected";
+  String _responseText = "";
+  String _pesticideText = "";
+  String _error = "";
+  bool _isLoading = false;
+
   final ImagePicker _imagePicker = ImagePicker();
 
   void _removeFile() {
     setState(() {
       _capturedImage = null;
       _fileName = "No file selected";
+      _responseText = "";
+      _pesticideText = "";
+      _error = "";
     });
   }
 
@@ -42,8 +55,154 @@ class _CameraAndFilePickerButtonState extends State<CameraAndFilePickerButton> {
     if (result != null) {
       setState(() {
         _fileName = result.files.single.name;
+        _capturedImage = File(result.files.single.path!);
       });
     }
+  }
+
+  void _showModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Prescription😁',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Text(
+                _responseText,
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Severity: 48 %",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              Text(
+                _pesticideText,
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Close the modal when the button in the modal is pressed
+                  Navigator.pop(context);
+                },
+                child: Text('Close'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  final Map<String, String> diseasesToFertilizers = {
+    // Apple diseases and corresponding fertilizers
+    "Apple___Apple_scab": "ScabShield",
+    "Apple___Black_rot": "RotGuard",
+    "Apple___Cedar_apple_rust": "RustShield",
+    "Apple___healthy": "AppleGrow+",
+
+    // Blueberry diseases and corresponding fertilizers
+    "Blueberry___healthy": "BlueberryBoost",
+
+    // Cherry diseases and corresponding fertilizers
+    "Cherry_(including_sour)___Powdery_mildew": "FungusFighter",
+    "Cherry_(including_sour)___healthy": "CherryChampion+",
+
+    // Corn diseases and corresponding fertilizers
+    "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot": "CornGuard+",
+    "Corn_(maize)__Common_rust": "RustShield",
+    "Corn_(maize)___Northern_Leaf_Blight": "NPK Blend+",
+    "Corn_(maize)___healthy": "BlightBlocker+",
+
+    // Grape diseases and corresponding fertilizers
+    "Grape___Black_rot": "vineVigour+",
+    "Grape__Esca(Black_Measles)": "VineShield",
+
+    // Tomato diseases and corresponding fertilizers
+    "Tomato___Bacterial_spot": "TomatoGuard+",
+    "Tomato___Early_blight": "TomatoShield+",
+    "Tomato___Late_blight": "TomatoDefender+",
+    // Add more disease-fertilizer mappings as needed
+  };
+
+  Future<void> _callApiWithImage(File imageFile) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final apiUrl = Uri.parse(
+        'https://thunderpytho.azurewebsites.net/api/pythodoc?code=TC9SKZeFOn_SZxEKaGhkpJk_-gYjg21IKtYJRdXbtvTdAzFuJyu8-w==');
+
+    try {
+      final request = http.MultipartRequest('POST', apiUrl);
+      print(imageFile.path);
+      request.files
+          .add(await http.MultipartFile.fromPath('crop_image', imageFile.path));
+
+      final response = await request.send();
+      if (response.statusCode == 200) {
+        final responseString = await response.stream.bytesToString();
+        print('API Response: $responseString');
+
+        // Parse the response to get the disease and pesticide details
+        final String pesticide =
+            diseasesToFertilizers[responseString] ?? 'Unknown Pesticide';
+
+        setState(() {
+          _responseText = 'Infection : $responseString';
+          _pesticideText = 'Pesticide: $pesticide';
+          _isLoading = false;
+        });
+
+        // Show the response in a modal
+        _showModal(context);
+      } else {
+        print('API Request failed with status code: ${response.statusCode}');
+        setState(() {
+          _error = 'API Request failed';
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      print('Error making API request: $e');
+      setState(() {
+        _error = 'Error making API request';
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _showAboutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('CropDoc - About'),
+          content: Text('This app helps identify crop diseases and recommends appropriate pesticides.'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -52,11 +211,54 @@ class _CameraAndFilePickerButtonState extends State<CameraAndFilePickerButton> {
       appBar: AppBar(
         title: Text('CropDoc'),
         backgroundColor: Colors.green, // Change the AppBar's background color
+        actions: [
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () {
+              _showAboutDialog(context);
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              width: 200, // Set the desired width here
+              height: 200, // Set the desired height here
+              child: Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  if (_capturedImage != null)
+                    Positioned.fill(
+                      child: Image.file(_capturedImage!, fit: BoxFit.cover),
+                    ),
+                  if (_capturedImage != null)
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Tooltip(
+                        message: 'Remove',
+                        child: ElevatedButton(
+                          onPressed: _removeFile,
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.red,
+                            onPrimary: Colors.white,
+                            minimumSize: Size(32, 32),
+                            padding: EdgeInsets.zero,
+                            shape: CircleBorder(),
+                          ),
+                          child: Icon(Icons.close),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+
+            ),
+
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -80,49 +282,35 @@ class _CameraAndFilePickerButtonState extends State<CameraAndFilePickerButton> {
                 ),
               ],
             ),
-
-            SizedBox(height: 10),
-            _capturedImage != null
-                ? Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Image.file(_capturedImage!), // Display the captured image
-                Positioned(
-                  top: 8,
-                  right: 8,
-                  child: Tooltip(
-                    message: 'Remove',
-                    child: ElevatedButton(
-                      onPressed: _removeFile, // Trigger the remove action
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.red, // Use red color for remove button
-                        onPrimary: Colors.white,
-                        minimumSize: Size(32, 32),
-                        padding: EdgeInsets.zero,
-                        shape: CircleBorder(),
-                      ),
-                      child: Icon(Icons.close),
-                    ),
-                  ),
-                ),
-              ],
-            )
-                : Text('No image/file selected'), // Show a message if no image/file is selected
             SizedBox(height: 10),
             Text(
               'Selected file: $_fileName',
               style: TextStyle(fontSize: 16, color: Colors.black),
             ),
             ElevatedButton(
-              onPressed: _captureImage,
+              onPressed: _capturedImage != null && !_isLoading
+                  ? () => _callApiWithImage(_capturedImage!)
+                  : null,
               style: ElevatedButton.styleFrom(
                 primary: Colors.green,
                 onPrimary: Colors.white,
               ),
-              child: Text('Detect'),
+              child: _isLoading
+                  ? CircularProgressIndicator()
+                  : Text('Detect'),
+            ),
+            Text(
+              '$_error',
+              style: TextStyle(fontSize: 16, color: Colors.black),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _removeFile,
+        label: Text('Reset'),
+        icon: Icon(Icons.refresh),
+        backgroundColor: Colors.red,
       ),
     );
   }
